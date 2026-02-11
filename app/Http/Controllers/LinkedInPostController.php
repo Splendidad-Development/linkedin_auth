f8d9082ecc53a2c1cdf2dd3dbaaf29d29d00ceba
abdulla-samii <abdulla.sami@splendidad.com>
2026-02-12 00:42:08 +0200

    fix image passing to linkedin api

--- app/Http/Controllers/LinkedInPostController.php
+++ app/Http/Controllers/LinkedInPostController.php
@@ -54,8 +54,19 @@ Excited for what’s ahead.
                 throw new Exception('LinkedIn access token has expired. Please reconnect your account.');
             }
 
-            $imageAbsolutePath = public_path('images/campaign-image.jpg');
-            $dedupHash = hash('sha256', $request->caption.'|'.(file_exists($imageAbsolutePath) ? sha1_file($imageAbsolutePath) : 'no-image'));
+            $imageAbsolutePath = null;
+            $candidateImagePaths = [
+                public_path('images/campaign-image.png'),
+                public_path('images/campaign-image.jpg'),
+            ];
+            foreach ($candidateImagePaths as $candidatePath) {
+                if (file_exists($candidatePath)) {
+                    $imageAbsolutePath = $candidatePath;
+                    break;
+                }
+            }
+
+            $dedupHash = hash('sha256', $request->caption.'|'.($imageAbsolutePath ? sha1_file($imageAbsolutePath) : 'no-image'));
             if ($linkedinAccount->last_post_hash === $dedupHash) {
                 throw new Exception('Duplicate post detected. Please change the caption or wait before posting again.');
             }
@@ -63,7 +74,7 @@ Excited for what’s ahead.
             $imageUrn = null;
 
             $authorUrn = 'urn:li:person:' . $linkedinAccount->linkedin_id;
-            if (file_exists($imageAbsolutePath)) {
+            if ($imageAbsolutePath) {
                 $imageUrn = $this->linkedInService->uploadImage($linkedinAccount->access_token, $authorUrn, $imageAbsolutePath);
             }
 
